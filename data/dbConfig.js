cabd303320cb223eb246b261e2fb7c4c2ea2d71f
cecil john tantay <cjbtantay@gmail.com>
2019-02-27 00:42:49 -0800

    pg1

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const knex = require('knex');
 const knexConfig = require('../knexfile');
-const environment = process.env.ENVIRONMENT || development;
+const environment = process.env.ENVIRONMENT || 'development';
 const db = knex(knexConfig[environment]);
 
 module.exports = db;
